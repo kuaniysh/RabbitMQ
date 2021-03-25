6d52a264f0fadfbcd8a078bcfc701d7ac5ed3f2c
KBekturganov <KBekturganov@beeline.kz>
2021-03-25 16:19:43 +0600

    Added tests

--- src/test/java/kz/beeline/rabbitMQ/topic/module/RabbitmqMockerTest.java
+++ src/test/java/kz/beeline/rabbitMQ/topic/module/RabbitmqMockerTest.java
@@ -2,40 +2,31 @@ package kz.beeline.rabbitMQ.topic.module;
 
 import com.github.fridujo.rabbitmq.mock.MockConnection;
 import com.github.fridujo.rabbitmq.mock.MockConnectionFactory;
+import com.rabbitmq.client.Channel;
 import com.rabbitmq.client.Connection;
 import com.rabbitmq.client.ConnectionFactory;
-import kz.beeline.rabbitMQ.topic.sender.TopicSender;
 import org.junit.jupiter.api.Test;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 import java.io.IOException;
 import java.util.concurrent.TimeoutException;
 
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.instanceOf;
-import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.*;
 
 @SpringBootTest
+@ActiveProfiles("dev")
 public class RabbitmqMockerTest {
 
     @Autowired
     RabbitTemplate template;
 
-    @Autowired
-    private TopicSender topicSender;
-
     @Test
-    void close_closes_connection() throws IOException {
-        try (Connection connection = new MockConnectionFactory().newConnection()) {
-            connection.close();
-            assertThat(connection.isOpen(), is(false));
-        }
-    }
-
-    @Test
-    void configure_by_params() throws IOException, TimeoutException {
+    void mockConnectionFactoryTest() throws IOException, TimeoutException {
         ConnectionFactory factory = new MockConnectionFactory();
 
         factory.setUsername("guest");
@@ -46,5 +37,32 @@ public class RabbitmqMockerTest {
 
         Connection connection = factory.newConnection();
         assertThat(connection, instanceOf(MockConnection.class));
+        System.out.println(connection.isOpen());
+
+        connection.close();
+        System.out.println(connection.isOpen());
+        assertThat(connection.isOpen(), is(false));
+    }
+
+    @Test
+    void channelTest() throws IOException, TimeoutException {
+        try (Connection conn = new MockConnectionFactory().newConnection()) {
+            try (Channel channel = conn.createChannel()) {
+                channel.close();
+                assertThat(channel.isOpen(), is(false));
+            }
+        }
+    }
+
+    @Test
+    void queueDelete_deletes_it() throws IOException, TimeoutException {
+        try (Connection conn = new MockConnectionFactory().newConnection()) {
+            try (Channel channel = conn.createChannel()) {
+                assertThat(channel.queueDeclare(), is(notNullValue()));
+                assertThat(channel.queueDelete(""), is(notNullValue()));
+                assertThatExceptionOfType(IOException.class)
+                        .isThrownBy(() -> channel.queueDeclarePassive(""));
+            }
+        }
     }
 }
